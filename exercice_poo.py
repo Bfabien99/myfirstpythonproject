0ae22647f9158f4dadd84268b83c632c46f56db6
bfabien99 <fabienbrou99@gmail.com>
2022-12-11 13:18:22 +0000

    methodes demander nom

--- exercice_poo.py
+++ exercice_poo.py
@@ -1,18 +1,28 @@
 class Personne:
-    def __init__(self, nom: str, age: int):
+    def __init__(self, nom: str = "", age: int = 0):
         self.nom = nom
-        self.age = age
+        self.age = int(age)
+        if nom == "":
+            self.DemanderNom()
 
     def SePresenter(self):
-        print("Bonjour, je m'appelle", self.nom, ", j'ai", self.age, "ans")
-        if self.EstMajeur():
-            print("Je suis majeur")
+        info_personne = "bonjour, je m'appelle {}".format(self.nom)
+        if self.age <= 0:
+            print(info_personne)
         else:
-            print("Je suis mineur")
+            age_personne = ", j'ai {} ans".format(self.age)
+            print(f"{info_personne}{age_personne}")
+            if self.EstMajeur():
+                print("Je suis Majeur")
+            else:
+                print("Je suis Mineur")
+
+    def DemanderNom(self):
+        self.nom = input("Quel est ton nom ? : ")
 
     def EstMajeur(self):
         return self.age >= 18
 
 
-personne = Personne("Fabien", 30)
+personne = Personne("", 12)
 personne.SePresenter()
