bd9a0532696d66ed743a30e3fce1f21cb6d16151
bfabien99 <fabienbrou99@gmail.com>
2022-12-11 14:47:22 +0000

    mise en situation 4, correction

--- ms4poo.py
+++ ms4poo.py
@@ -6,17 +6,21 @@ class Personne:
         print("Bonjour, je suis une personne et je m'apelle " + self.nom)
 
 
+nombre_de_personnes = 3
+"""
 noms = []
-i = 1
-while True:
-    nom = input("Entrer le nom de la personne {} : ".format(i))
-    if nom == "":
-        break
-    i += 1
-    noms.append(nom)
+
+for i in range(nombre_de_personnes):
+    noms.append(input("Entrer le nom de la personne {} : ".format(i+1)))
+
 
 liste = []
 for nom in noms:
+    liste.append(Personne(nom))
+"""
+liste = []
+for i in range(nombre_de_personnes):
+    nom = input("Entrer le nom de la personne {} : ".format(i+1))
     liste.append(Personne(nom))
 
 for personne in liste:
