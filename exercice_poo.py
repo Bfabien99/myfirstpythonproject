52d0c89a707918c7b88652110d7bdad99678758e
bfabien99 <fabienbrou99@gmail.com>
2022-12-11 12:55:17 +0000

    methodes se presenter et est majeur

--- exercice_poo.py
+++ exercice_poo.py
@@ -1,10 +1,17 @@
 class Personne:
-    def __init__(self, nom, age):
+    def __init__(self, nom: str, age: int):
         self.nom = nom
         self.age = age
 
     def SePresenter(self):
         print("Bonjour, je m'appelle", self.nom, ", j'ai", self.age, "ans")
+        if self.EstMajeur():
+            print("Je suis majeur")
+        else:
+            print("Je suis mineur")
+
+    def EstMajeur(self):
+        return self.age >= 18
 
 
 personne = Personne("Fabien", 30)
