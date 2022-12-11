216a122770fcf884310b460eee96be80f6cd63e7
bfabien99 <fabienbrou99@gmail.com>
2022-12-11 13:48:00 +0000

    mise en situation 1 correction

--- ms1poo.py
+++ ms1poo.py
@@ -1,28 +1,30 @@
 class Personne:
-    def __init__(self, nom:str, age:int, genre:bool):
+    def __init__(self, nom: str, age: int, genre: bool):
         self.nom = nom
         self.age = age
         self.genre = genre
 
     def sePresenter(self):
+        print("Bonjour, je m'appelle {}, j'ai {} ans".format(self.nom, self.age))
+        e_optionnel = ""
         if self.genre:
-            print("Bonjour, je m'appelle %s, j'ai %s ans" % self.nom % self.age)
-            print("Genre : Homme")
-            if self.EstMajeur():
-                print("Je suis Majeur")
-            else:
-                print("Je suis Mineur")
-
+            genre_str = "Homme"
         else:
-            print("Bonjour, je m'appelle %s, j'ai %s ans" % self.nom % self.age)
-            print("Genre : Femme")
-            if self.EstMajeur():
-                print("Je suis Majeure")
-            else:
-                print("Je suis Mineure")
+            genre_str = "Femme"
+            e_optionnel = "e"
+        print("Genre : {}".format(genre_str))
+
+        # genre_str = "Homme" if self.genre else "Femme"
+        # e_optionnel = "" if self.genre else "e"
+
+        if self.EstMajeur():
+            print("Je suis Majeur" + e_optionnel)
+        else:
+            print("Je suis Mineur" + e_optionnel)
 
     def EstMajeur(self):
         return self.age >= 18
 
 
-personne = Personne("Fabien", 24)
+personne = Personne("Fabien", 24, True)
+personne.sePresenter()
