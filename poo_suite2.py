55af53f852d91425d5e2306e903f72e3dfa2e914
bfabien99 <fabienbrou99@gmail.com>
2022-12-11 15:31:22 +0000

    surchage de la methode sePresenter

--- poo_suite2.py
+++ poo_suite2.py
@@ -38,35 +38,16 @@ class Personne(Espece):
         return self.age >= 18
 
 
-class Etudiant:
+class Etudiant(Personne):
     def __init__(self, nom: str = "", age: int = 0, etude: tuple = ()):
         self.nom = nom
         self.age = int(age)
         self.etude = etude
 
-    def SePresenter(self):
-        info_personne = "bonjour, je m'appelle {}".format(self.nom)
-        if self.age <= 0:
-            print(info_personne)
-        else:
-            age_personne = ", j'ai {} ans".format(self.age)
-            print(f"{info_personne}{age_personne}")
-            if self.EstMajeur():
-                print("Je suis Majeur")
-            else:
-                print("Je suis Mineur")
-
-    def DemanderNom(self):
-        self.nom = input("Quel est ton nom ? : ")
-
-    def EstMajeur(self):
-        return self.age >= 18
-
-    def AfficherEtude(self):
-        print(" -->> Liste etude <<-- ")
+    def SePresenter(self): # surchage de la méthode
+        print(" -->> Je suis étudiant en <<-- ")
         for etude in self.etude:
             print(etude)
 
 
-Personne("Aline", 19).AfficherEspece()
-Etudiant("Brou", 21).AfficherEtude()
+Etudiant("Brou", 21, ("Math", "Chimie")).SePresenter()
